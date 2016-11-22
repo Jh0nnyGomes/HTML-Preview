4c1c4efe8513e32a18e71722c8411555ed613b5e
Jh0nnyGomes <gomesdepaula.jhonnathan@itisriva.gov.it>
2016-11-22 10:07:22 +0100

    HTML Preview_2

--- HTML Preview/HTML Preview/Form1.Designer.cs
+++ HTML Preview/HTML Preview/Form1.Designer.cs
@@ -28,12 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txt_html = new System.Windows.Forms.TextBox();
+            this.txt_css = new System.Windows.Forms.TextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // txt_html
+            // 
+            this.txt_html.Location = new System.Drawing.Point(2, 2);
+            this.txt_html.Multiline = true;
+            this.txt_html.Name = "txt_html";
+            this.txt_html.Size = new System.Drawing.Size(86, 20);
+            this.txt_html.TabIndex = 0;
+            // 
+            // txt_css
+            // 
+            this.txt_css.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.txt_css.Location = new System.Drawing.Point(94, 2);
+            this.txt_css.Multiline = true;
+            this.txt_css.Name = "txt_css";
+            this.txt_css.Size = new System.Drawing.Size(94, 20);
+            this.txt_css.TabIndex = 1;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(194, 2);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(89, 20);
+            this.webBrowser1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.txt_css);
+            this.Controls.Add(this.txt_html);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txt_html;
+        private System.Windows.Forms.TextBox txt_css;
+        private System.Windows.Forms.WebBrowser webBrowser1;
     }
 }
 
--- HTML Preview/HTML Preview/Form1.cs
+++ HTML Preview/HTML Preview/Form1.cs
@@ -16,5 +16,25 @@ namespace HTML_Preview
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+
+            txt_html.Height = Height;
+            txt_html.Width = Width / 3;
+
+            txt_css.Height = Height;
+            txt_css.Width = Width / 3;
+
+            webBrowser1.Height = Height;
+            webBrowser1.Width = Width / 3;
+
+            txt_html.Location = new Point(10, 10);
+
+            txt_css.Location = new Point((Width / 3), 10);
+
+            webBrowser1.Location = new Point(((Width / 3) * 2), 10);
+        }
     }
 }
