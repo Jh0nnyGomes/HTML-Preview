1c470d439b3dc34299ac8630b9f19bf48f26e129
J <olivato.julian@itisriva.gov.it>
2016-11-25 19:24:16 +0100

    Implementazione Gestione file

---
+++ HTML Preview/HTML-Preview/HTML Preview/HTML Preview/Editor.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace HTML_Preview
+{
+    partial class Editor
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Liberare le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_html = new System.Windows.Forms.TextBox();
+            this.txt_css = new System.Windows.Forms.TextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileMenuStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.apriToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salvaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.chiudiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.strumentiMenuStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.TemiMenuStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.chiaroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.scuroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_new = new System.Windows.Forms.Button();
+            this.btn_load = new System.Windows.Forms.Button();
+            this.btn_continue = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txt_html
+            // 
+            this.txt_html.Location = new System.Drawing.Point(2, 34);
+            this.txt_html.Multiline = true;
+            this.txt_html.Name = "txt_html";
+            this.txt_html.Size = new System.Drawing.Size(86, 20);
+            this.txt_html.TabIndex = 0;
+            // 
+            // txt_css
+            // 
+            this.txt_css.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.txt_css.Location = new System.Drawing.Point(94, 34);
+            this.txt_css.Multiline = true;
+            this.txt_css.Name = "txt_css";
+            this.txt_css.Size = new System.Drawing.Size(94, 20);
+            this.txt_css.TabIndex = 1;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(194, 34);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(89, 20);
+            this.webBrowser1.TabIndex = 2;
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.BackColor = System.Drawing.SystemColors.Control;
+            this.menuStrip.Dock = System.Windows.Forms.DockStyle.None;
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileMenuStrip,
+            this.strumentiMenuStrip,
+            this.TemiMenuStrip});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(161, 24);
+            this.menuStrip.TabIndex = 3;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // fileMenuStrip
+            // 
+            this.fileMenuStrip.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.fileMenuStrip.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.apriToolStripMenuItem,
+            this.salvaToolStripMenuItem,
+            this.chiudiToolStripMenuItem});
+            this.fileMenuStrip.Name = "fileMenuStrip";
+            this.fileMenuStrip.Size = new System.Drawing.Size(37, 20);
+            this.fileMenuStrip.Text = "File";
+            // 
+            // apriToolStripMenuItem
+            // 
+            this.apriToolStripMenuItem.BackColor = System.Drawing.Color.Transparent;
+            this.apriToolStripMenuItem.Name = "apriToolStripMenuItem";
+            this.apriToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.apriToolStripMenuItem.Text = "Apri";
+            // 
+            // salvaToolStripMenuItem
+            // 
+            this.salvaToolStripMenuItem.BackColor = System.Drawing.Color.Transparent;
+            this.salvaToolStripMenuItem.Name = "salvaToolStripMenuItem";
+            this.salvaToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.salvaToolStripMenuItem.Text = "Salva";
+            // 
+            // chiudiToolStripMenuItem
+            // 
+            this.chiudiToolStripMenuItem.BackColor = System.Drawing.Color.Transparent;
+            this.chiudiToolStripMenuItem.Name = "chiudiToolStripMenuItem";
+            this.chiudiToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.chiudiToolStripMenuItem.Text = "Chiudi";
+            // 
+            // strumentiMenuStrip
+            // 
+            this.strumentiMenuStrip.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.strumentiMenuStrip.Name = "strumentiMenuStrip";
+            this.strumentiMenuStrip.Size = new System.Drawing.Size(71, 20);
+            this.strumentiMenuStrip.Text = "Strumenti";
+            // 
+            // TemiMenuStrip
+            // 
+            this.TemiMenuStrip.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.TemiMenuStrip.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.chiaroToolStripMenuItem,
+            this.scuroToolStripMenuItem});
+            this.TemiMenuStrip.Name = "TemiMenuStrip";
+            this.TemiMenuStrip.Size = new System.Drawing.Size(45, 20);
+            this.TemiMenuStrip.Text = "Temi";
+            // 
+            // chiaroToolStripMenuItem
+            // 
+            this.chiaroToolStripMenuItem.BackColor = System.Drawing.Color.Transparent;
+            this.chiaroToolStripMenuItem.Name = "chiaroToolStripMenuItem";
+            this.chiaroToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.chiaroToolStripMenuItem.Text = "Chiaro";
+            this.chiaroToolStripMenuItem.Click += new System.EventHandler(this.chiaroToolStripMenuItem_Click);
+            // 
+            // scuroToolStripMenuItem
+            // 
+            this.scuroToolStripMenuItem.AutoToolTip = true;
+            this.scuroToolStripMenuItem.BackColor = System.Drawing.Color.Transparent;
+            this.scuroToolStripMenuItem.Name = "scuroToolStripMenuItem";
+            this.scuroToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.scuroToolStripMenuItem.Text = "Scuro";
+            this.scuroToolStripMenuItem.Click += new System.EventHandler(this.scuroToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(553, 180);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 4;
+            // 
+            // btn_new
+            // 
+            this.btn_new.FlatAppearance.BorderSize = 0;
+            this.btn_new.Location = new System.Drawing.Point(159, 305);
+            this.btn_new.Name = "btn_new";
+            this.btn_new.Size = new System.Drawing.Size(107, 23);
+            this.btn_new.TabIndex = 5;
+            this.btn_new.Text = "Nuovo Progetto";
+            this.btn_new.UseVisualStyleBackColor = true;
+            // 
+            // btn_load
+            // 
+            this.btn_load.Location = new System.Drawing.Point(286, 304);
+            this.btn_load.Name = "btn_load";
+            this.btn_load.Size = new System.Drawing.Size(75, 23);
+            this.btn_load.TabIndex = 6;
+            this.btn_load.Text = "Carica";
+            this.btn_load.UseVisualStyleBackColor = true;
+            // 
+            // btn_continue
+            // 
+            this.btn_continue.Location = new System.Drawing.Point(380, 305);
+            this.btn_continue.Name = "btn_continue";
+            this.btn_continue.Size = new System.Drawing.Size(75, 23);
+            this.btn_continue.TabIndex = 7;
+            this.btn_continue.Text = "Continua";
+            this.btn_continue.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(509, 426);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 100);
+            this.panel2.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1028, 544);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.btn_continue);
+            this.Controls.Add(this.btn_load);
+            this.Controls.Add(this.btn_new);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.txt_css);
+            this.Controls.Add(this.txt_html);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_html;
+        private System.Windows.Forms.TextBox txt_css;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem apriToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salvaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem chiudiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem strumentiMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem TemiMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem chiaroToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem scuroToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn_new;
+        private System.Windows.Forms.Button btn_load;
+        private System.Windows.Forms.Button btn_continue;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ HTML Preview/HTML-Preview/HTML Preview/HTML Preview/Editor.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FileManagement;
+
+namespace HTML_Preview
+{
+    public partial class Editor : Form
+    {
+        #region Grafica
+
+        Color BGColorDark = System.Drawing.ColorTranslator.FromHtml("#171616"); 
+        Color TxtBoxColorDark = System.Drawing.ColorTranslator.FromHtml("#272626");
+        Color TextColorWhite = System.Drawing.ColorTranslator.FromHtml("#fff");
+
+        Color BGColorLight = System.Drawing.ColorTranslator.FromHtml("#ececec");
+        Color TxtBoxColorLight = System.Drawing.ColorTranslator.FromHtml("#fff");
+        Color TextColorBlack = System.Drawing.ColorTranslator.FromHtml("#000");
+
+        #endregion
+
+        FileManager FM = new FileManager();
+        string[] filePaths;
+        public string ProjectPath;
+
+        public Editor()
+        {
+            InitializeComponent();
+            FM.ResourceInitialize();//inizializza il file di risorse
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            #region Grafica
+
+            this.WindowState = FormWindowState.Maximized;
+
+            this.BackColor = BGColorDark;
+
+            btn_new.FlatStyle = FlatStyle.Flat;
+            btn_new.BackColor = Color.Transparent;
+            btn_new.ForeColor = TextColorWhite;
+
+            panel1.Dock = DockStyle.Fill;
+            panel1.Controls.Add(btn_new);
+            panel1.Controls.Add(btn_load);
+            panel1.Controls.Add(btn_continue);
+
+            panel2.Hide();
+
+            //Posizionamento, Stile, Dimensioni TextBox dell'HTML
+            txt_html.Height = Height - 100;
+            txt_html.Width = Width / 3 - 20;
+            txt_html.BackColor = TxtBoxColorDark;
+            txt_html.ForeColor = TextColorWhite;
+            txt_html.BorderStyle = BorderStyle.None;
+            txt_html.Font = new Font("Cambria", 16);
+            txt_html.Location = new Point(30, 30);
+
+            //Posizionamento, Stile, Dimensioni TextBox del CSS
+            txt_css.Height = Height - 100;
+            txt_css.Width = Width / 3 - 20;
+            txt_css.BackColor = TxtBoxColorDark;
+            txt_css.ForeColor = TextColorWhite;
+            txt_css.BorderStyle = BorderStyle.None;
+            txt_css.Font = new Font("Cambria", 16);
+            txt_css.Location = new Point((Width / 3) + 15, 30);
+
+            //Posizionamento e Dimensioni del visualizzatore WEB
+            webBrowser1.Height = Height - 100;
+            webBrowser1.Width = Width / 3 - 40;
+            webBrowser1.Location = new Point(((Width / 3) * 2), 30);
+
+            //Personalizzazione del Menù
+            menuStrip.BackColor = Color.Transparent;
+            menuStrip.ForeColor = TextColorWhite;
+
+            scuroToolStripMenuItem.ToolTipText = "Predefinito";
+            
+            panel2.Dock = DockStyle.Fill;
+            panel2.Controls.Add(menuStrip);
+            panel2.Controls.Add(txt_html);
+            panel2.Controls.Add(txt_css);
+            panel2.Controls.Add(webBrowser1);
+
+#endregion 
+        }
+
+        #region Menu
+
+        private void chiaroToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.BackColor = BGColorLight;
+
+            txt_html.BackColor = TxtBoxColorLight;
+            txt_html.ForeColor = TextColorBlack;
+
+            txt_css.BackColor = TxtBoxColorLight;
+            txt_css.ForeColor = TextColorBlack;
+
+
+            menuStrip.ForeColor = TextColorBlack;
+        }
+
+        private void scuroToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.BackColor = BGColorDark;
+
+            txt_html.BackColor = TxtBoxColorDark;
+            txt_html.ForeColor = TextColorWhite;
+
+            txt_css.BackColor = TxtBoxColorDark;
+            txt_css.ForeColor = TextColorWhite;
+
+
+            menuStrip.ForeColor = TextColorWhite;
+        }
+
+        #endregion
+
+        //Aggiorna la Pagina quando si scrive
+        private void Refresh_OnKeyUp(object sender, KeyEventArgs e)
+        {
+            TextBox t = (TextBox)sender;
+            int i;//l'indice è 0 per html 1 per css
+
+            if (t.Name == txt_html.Name)
+                i = 0;
+            else
+                i = 1;
+
+            //aggiorna il file
+            if (!FM.TryToWrite(filePaths[i], txt_html.Text, false))
+            {
+                MessageBox.Show("Impossibile scrivere sul file", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            Wb_Preview.Navigate(filePaths[i]);
+        }
+
+        //carica progetto
+        private void Btn_LoadProject_Click(object sender, EventArgs e)//////////////////
+        {
+            if (PathDialog(out ProjectPath))
+            {
+            Extract:
+                if (Extract(ProjectPath, out fileContent))
+                    return;
+                else
+                {
+                    DialogResult dr = MessageBox.Show("file non trovati", "Error", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+                    if (dr == DialogResult.Retry)
+                        goto Extract;
+                }
+            }
+            else
+                return;
+        }
+
+        private void Btn_NewProject_Click(object sender, EventArgs e)
+        {
+            //sceglie la cartella in cui inserirlo
+            string[] path;
+            if (PathDialog(out path))
+            {
+                string title = null;
+                if (!FindTitle(out title))
+                    title = "Sample";
+
+                FM.FileAdd(title, ".html", txt_html.Text);
+                FM.FileAdd(title, ".html", txt_html.Text);
+            }
+        }
+
+        //continua dal progetto precedente
+        private void Btn_Continue_Click(object sender, EventArgs e)
+        {
+            //contenuto dei file html[0] e css[1]
+            string[] fileContent = new string[0];
+
+            //legge dalle risorse
+            List<string> resources = new List<string>();//contiene i dati del file di risorse
+            if (FM.ResourceReading(out resources))//se riesce a leggere il file 
+            {
+                if (Extract(resources[0], out fileContent))//se riesce ad estrarre i contenuti del file indirizzato dalle risorse
+                {
+                    txt_html.Text = fileContent[0];
+                    txt_css.Text = fileContent[1];
+                    return;
+                }
+            }
+            MessageBox.Show("Nessun Progetto in corso trovato", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        #region Gestione File
+
+        //estrae i file se li trova
+        private bool Extract(string path, out string[] file)
+        {
+            file = new string[2];//file html e css
+            //entrare dentro la cartella
+            string[] FilePath = System.IO.Directory.GetFiles(path);
+
+            //cercare i file html e css, [0] è html, [1] è css
+            int ok = 0;//numero di file trovati
+            for (int i = 0; i < FilePath.Length || ok != 2; i--)
+            {
+                string p = System.IO.Path.GetFileName(FilePath[i]);
+                if (FileManager.ItsExtensionIs(p, ".html"))
+                {
+                    file[0] = FileManager.ToString(FilePath[i]);
+                    ok++;
+                }
+                else if (FileManager.ItsExtensionIs(p, ".css"))
+                {
+                    file[1] = FileManager.ToString(FilePath[i]);
+                    ok++;
+                }
+            }
+            if (ok != 2)
+                return false;//se mancano i file
+            else
+                return true;
+        }
+
+        bool PathDialog(out string path)
+        {
+            path = null;
+            FolderBrowserDialog f = new FolderBrowserDialog();
+            if (f.ShowDialog() == DialogResult.OK)
+            {
+                path = f.SelectedPath;
+                return true;
+            }
+            return false;
+        }
+
+        bool PathDialog(out string[] path)
+        {
+            path = null;
+            OpenFileDialog f = new OpenFileDialog();
+            f.Filter = "html files (*.html)|*.html|css files (*.css)|*.css";
+            f.Title = "Carica i file";
+            if (f.ShowDialog() == DialogResult.OK)
+            {
+                path = f.FileNames;
+                return true;
+            }
+            return false;
+        }
+
+        #endregion
+
+        #region Gestione Textbox
+
+        bool FindTitle(out string title)
+        {
+            //ottiene la posizione iniziale del titolo
+            int index, end;
+            for (index = 0; index < txt_html.Text.Length - 7; index++)
+                if (txt_html.Text.Substring(index, 7).ToUpper() == "<TITLE>")
+                    break;
+            //ottiene la posizione finale
+            for (end = index; end < txt_html.Text.Length - 7; end++)
+                if (txt_html.Text.Substring(end, 7).ToUpper() == "</TITLE>")
+                    break;
+
+            title = txt_html.Text.Substring(index, end - index);
+            if (title != null)
+                return true;
+            else
+                return false;
+        }
+
+        #endregion
+    }
+}
